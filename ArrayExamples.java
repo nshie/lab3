0f131bedff7eedd46ec821b0d0bb330094922993
phloamy <njshie@gmail.com>
2024-02-01 17:26:25 -0800

    Update lab3

--- ArrayExamples.java
+++ ArrayExamples.java
@@ -4,9 +4,11 @@ public class ArrayExamples {
 
   // Changes the input array to be in reversed order
   static void reverseInPlace(int[] arr) {
-    for(int i = 0; i < arr.length; i += 1) {
-      arr[i] = arr[arr.length - i - 1];
+    int[] newArray = new int[arr.length];
+    for (int i = 0; i < arr.length; i += 1) {
+      newArray[i] = arr[arr.length - i - 1];
     }
+    arr = newArray;
   }
 
   // Returns a *new* array with all the elements of the input array in reversed
@@ -14,7 +16,7 @@ public class ArrayExamples {
   static int[] reversed(int[] arr) {
     int[] newArray = new int[arr.length];
     for(int i = 0; i < arr.length; i += 1) {
-      arr[i] = newArray[arr.length - i - 1];
+      newArray[i] = arr[arr.length - i - 1];
     }
     return arr;
   }
